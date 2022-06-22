14b76a9a62df45ab64077ca417fa026945a1549c
Mark Joseph Manongsong <markmanongsong05@gmail.com>
2022-06-22 18:32:29 +0800

    Initial commit

---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div className="w-[50px]"></div>
+
+      {/* menu */}
+      <ul className=" hidden md:flex font-sans">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+      {/* Mobile */}
+      <ul
+        className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+        }
+      >
+        <li className="py-6 text-4xl ">Home</li>
+        <li className="py-6 text-4xl ">About</li>
+        <li className="py-6 text-4xl ">Skills</li>
+        <li className="py-6 text-4xl ">Projects</li>
+        <li className="py-6 text-4xl ">Contact</li>
+      </ul>
+
+      {/* Social Icons */}
+      <div className="hidden"></div>
+    </div>
+  );
+};
+
+export default Navbar;
