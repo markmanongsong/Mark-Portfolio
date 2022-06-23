d9deadea66a58000fc2c649cd27344215b8b5742
Mark Joseph Manongsong <markmanongsong05@gmail.com>
2022-06-23 14:27:08 +0800

    updated version

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
+import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
-    <div name="home" className="w-full h-screen ">
+    <div name="home" className="w-full h-screen font-sans ">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-[#333333]">Hi, my name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[]">
-          Mark Joseph Manongsong
-        </h1>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[]">Mark</h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
           I'm a Full Stack Developer
         </h2>
         <p className="text-[#0a192f] py-4 max-w-[700px]">
-          I'm a fresh graduate Full Stack Web Developer. Learn web dev basics
-          within 6 months and produce 6 projects. varying from static to full
-          stack
+          I'm Mark Joseph Manongsong aiming to be a full stack web developer.
+          Currently, I’m focused on building responsive full-stack web
+          applications.
         </p>
         <div>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center bg-black hover:bg-slate-500  hover:text-white">
-            View Work
-            <span className="group-hover:rotate-90 duration-300">
-              <HiArrowNarrowRight className="ml-3 " />
-            </span>
-          </button>
+          <Link to="project" smooth={true} duration={500}>
+            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center bg-black hover:bg-slate-500  hover:text-white">
+              View Work
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="ml-3 " />
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -9,6 +9,7 @@ import {
 } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -18,12 +19,32 @@ const Navbar = () => {
       <div className="w-[50px]"></div>
 
       {/* menu */}
-      <ul className=" hidden md:flex font-sans">
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Projects</li>
-        <li>Contact</li>
+      <ul className="hidden md:flex font-sans">
+        <li>
+          <Link to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to="project" smooth={true} duration={500}>
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/* Hamburger */}
@@ -38,11 +59,35 @@ const Navbar = () => {
             : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
         }
       >
-        <li className="py-6 text-4xl ">Home</li>
-        <li className="py-6 text-4xl ">About</li>
-        <li className="py-6 text-4xl ">Skills</li>
-        <li className="py-6 text-4xl ">Projects</li>
-        <li className="py-6 text-4xl ">Contact</li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {' '}
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {' '}
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {' '}
+          <Link onClick={handleClick} to="project" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {' '}
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/* Social Icons */}
