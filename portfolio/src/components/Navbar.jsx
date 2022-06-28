9bd63c6f24938c56ccd0d17d6d2880fca0d4b72f
Mark Joseph Manongsong <markmanongsong05@gmail.com>
2022-06-28 17:51:08 +0800

    updated demo project water counter

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
         className={
           !nav
             ? 'hidden'
-            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center font-sans'
         }
       >
         <li className="py-6 text-4xl">
