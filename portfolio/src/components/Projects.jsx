9b36ad47116e949fbefb7bd33906e67e3b0c5ec3
Mark Joseph Manongsong <markmanongsong05@gmail.com>
2022-07-04 14:51:15 +0800

    added ecommerce code and demo

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -147,12 +147,18 @@ const Work = () => {
                 React JS and Tailwind css Application
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a
+                  href="https://markmanongsong.github.io/ecommerce-app"
+                  target="_blank"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a
+                  href="https://github.com/markmanongsong/Ecommerce_app"
+                  target="_blank"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
