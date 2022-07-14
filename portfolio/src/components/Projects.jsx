7bb504a6ec5fb4ce37270db34c048861e1e0e497
Mark Joseph Manongsong <markmanongsong05@gmail.com>
2022-07-14 21:51:36 +0800

    added weather app demo

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -101,7 +101,10 @@ const Work = () => {
                 React JS Application
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a
+                  href="https://markmanongsong.github.io/weather-app/"
+                  target="_blank"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
